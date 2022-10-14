52f1998a6e76c6c161eca0d1998a5afa1bde37cb
user1 <wjsdbdud327@naver.com>
2022-10-14 15:33:39 +0900

    upload HW2

--- HW2/student20201015.py
+++ HW2/student20201015.py
@@ -1,2 +1,55 @@
-print("Hello")
-print("edit test")
+#!/usr/bin/python3
+from openpyxl import load_workbook 
+
+import numpy as np
+
+wb = load_workbook(filename = 'student.xlsx')
+ws = wb['Sheet1']
+
+r = 1
+data = []
+for row in ws:
+	if r != 1:
+		data.append(ws.cell(row = r, column = 7).value)
+		#print(data)
+	r += 1
+lst = np.array(data)
+a = np.quantile(lst, 0.7)
+b = np.quantile(lst, 0.3)
+
+aLst = []
+bLst = []
+cLst = []
+for v in data:
+	if v >= a:
+		aLst.append(v)
+	elif v>= b:
+		bLst.append(v)
+	else:
+		cLst.append(v)
+
+aPlusCut = np.quantile(aLst, 0.5)
+bPlusCut = np.quantile(bLst, 0.5)
+cPlusCut = np.quantile(cLst, 0.5)
+
+r = 1
+for row in ws:
+	score = ws.cell(row = r, column = 7).value
+	if r != 1:
+		if score >= aPlusCut:
+			ws.cell(row = r, column = 8, value = "A+")
+		elif score >= a:
+			ws.cell(row = r, column = 8, value = "A0")
+		elif score >= bPlusCut:
+			ws.cell(row = r, column = 8, value = "B+")
+		elif score >= b:
+			ws.cell(row = r, column = 8, value = "B0")
+		elif score >= cPlusCut:
+			ws.cell(row = r, column = 8, value = "C+")
+		else:
+			ws.cell(row = r, column = 8, value = "C0")
+	r += 1
+
+wb.save("student.xlsx")
+		
+
