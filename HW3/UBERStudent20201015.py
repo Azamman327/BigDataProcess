7a1d6bee93cce89c43f780c4bc1bfc1158f0599d
user1 <wjsdbdud327@naver.com>
2022-11-02 06:50:33 +0900

    upload uber

---
+++ HW3/UBERStudent20201015.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+import sys
+import calendar
+
+rslt = []
+dayofweek = ['MON', 'TUE', 'WED', 'THU', 'FRI', 'SAT', 'SUN']
+f = open(sys.argv[1])
+
+for line in f:
+	list = line.split(",")
+
+	date = list[1].split("/")
+	#print(date)
+	day = calendar.weekday(int(date[2]), int(date[0]), int(date[1]))
+	#print(dayofweek[day])
+	list[1] = dayofweek[day]
+
+	list[3] = list[3].replace("\n", "")
+
+	rslt.append(list)
+#print(rslt)
+wf = open(sys.argv[2], "wt")
+for uLine in rslt:
+	wf.write(uLine[0] + "," + uLine[1] + "\t" + uLine[2] + "," + uLine[3] + "\n")
+
+f.close()
+wf.close()
