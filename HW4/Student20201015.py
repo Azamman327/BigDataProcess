f356efa19fbd2d85ddea4215d4647e3817564a3f
user1 <wjsdbdud327@naver.com>
2022-12-23 22:27:14 +0900

    upload HW4

---
+++ HW4/Student20201015.py
@@ -0,0 +1,82 @@
+#!/usr/bin/python3
+
+import numpy as np
+import operator
+from pathlib import Path
+
+def createDataSet(filePath):
+	fileNames = []
+	for file in Path(filePath).iterdir():
+		fileNames.append(str(file))	
+	
+	labels = []
+	for v in fileNames:
+		temp = v.split('/')
+		labelName = temp[1][0]
+		labels.append(labelName)
+
+	data = []
+	line = []
+	for i in range(0, len(fileNames)):
+		group = []
+		file = open(fileNames[i], "r")
+		content = file.read()
+		for w in content:
+			if (w != '\n'):
+				group.append(int(w))
+		data.append(group)
+	#print(data)
+	dataList = np.array(data)
+
+	file.close()
+	return dataList, labels
+
+def classify0(inX, dataSet, labels, k):
+	dataSetSize = dataSet.shape[0]
+	diffMat = np.tile(inX, (dataSetSize, 1)) - dataSet
+	sqDiffMat = diffMat ** 2
+	sqDistances = sqDiffMat.sum(axis = 1)
+	distances = sqDistances ** 0.5	
+	sortedDistIndicies = distances.argsort()
+	classCount = {}
+	for i in range(k):
+		voteIlabel = labels[sortedDistIndicies[i]]
+		classCount[voteIlabel] = classCount.get(voteIlabel, 0) + 1
+#	print(classCount)
+	sortedClassCount = sorted(classCount.items(),
+		key = operator.itemgetter(1), reverse = True)
+	return sortedClassCount[0][0]
+
+filePath = 'trainingDigits'
+traindata, trainLabels = createDataSet(filePath)
+#print(traindata[0])
+#print(trainLabels)
+
+fileLength = 0
+for k in range(1, 21):
+	dir = 'testDigits'
+	testGroup = []
+	testLabels = []
+	data = []
+
+	errorCount = 0
+	for file in Path(dir).iterdir():
+		fileLength += 1
+		fileName = str(file)
+		testLabels.append(fileName.split('_'))
+		
+		f = open(file, "r")
+		content = f.read()
+		group = []
+		for w in content:
+			if (w != '\n'):
+				group.append(int(w))
+		inX = np.array(group)
+		#print(inX)
+		rslt = classify0(inX, traindata, trainLabels, k)
+
+		if (testLabels[-1] != rslt):
+			errorCount += 1	
+
+	errorRate = errorCount / fileLength * 100
+	print(int(errorRate))		
